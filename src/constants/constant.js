0992af22a6d7bcaee24b28f10f9daa28e23f0056
Rana Pratap <ranapratap.bitmesra@gmail.com>
2019-10-01 20:27:43 +0530

    added new banner

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,11 +1,13 @@
 import Invixtos from '../assets/Dashboard/Inviktus.jpg';
-import img1 from '../assets/Dashboard/shutterstock_518466241.jpg';
-import img2 from '../assets/Dashboard/shutterstock_530748871.jpg';
+import img1 from '../assets/Dashboard/Analytics.jpg';
+import img2 from '../assets/Dashboard/Analytics_Network.jpg';
+import img3 from '../assets/Dashboard/Good_Analytics.jpg';
 var data = {
     images : [
         Invixtos,
         img1,
-        img2
+        img2,
+        img3,
     ],
 };
 
